2434104f866560b49e91454bb3cfd60ca198d33c
shinchan <akashkumar3432@gmail.com>
2021-01-04 12:25:25 +0530

    Updates

---
+++ precache-manifest.abfde994ab3a7f5de8c0cd8cea34abea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359b37c25a3a745a730849b310d4d568",
+    "url": "/faq-card/index.html"
+  },
+  {
+    "revision": "77df3e74927eb754753e",
+    "url": "/faq-card/static/css/main.266d1093.chunk.css"
+  },
+  {
+    "revision": "0abff1486a969b092489",
+    "url": "/faq-card/static/js/2.1280ef07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faq-card/static/js/2.1280ef07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77df3e74927eb754753e",
+    "url": "/faq-card/static/js/main.857c4cd1.chunk.js"
+  },
+  {
+    "revision": "4195c9318e4b336308f6",
+    "url": "/faq-card/static/js/runtime-main.410362e3.js"
+  },
+  {
+    "revision": "ba0110cbe699febf9b554567b3c319ab",
+    "url": "/faq-card/static/media/arrowDown.ba0110cb.svg"
+  },
+  {
+    "revision": "a2580ee90560fc8105341c37e970974a",
+    "url": "/faq-card/static/media/bgDesktop.a2580ee9.svg"
+  },
+  {
+    "revision": "40d8475d9d575db6640d7db693df351f",
+    "url": "/faq-card/static/media/bgMobile.40d8475d.svg"
+  },
+  {
+    "revision": "a7ef46ea71d879197334a0d3b77938b2",
+    "url": "/faq-card/static/media/box.a7ef46ea.svg"
+  },
+  {
+    "revision": "44e3065026b08ed32221e1916da7484e",
+    "url": "/faq-card/static/media/womanDesktop.44e30650.svg"
+  },
+  {
+    "revision": "daf669348cc4af537c1224c0b07de357",
+    "url": "/faq-card/static/media/womanMobile.daf66934.svg"
+  }
+]);
